e829ed156e04a265558f4d60319dca2fdf92745c
Almaz <59019588+sharshenaliev@users.noreply.github.com>
2023-03-05 12:24:57 +0600

    beta

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,7 @@ PARLER_LANGUAGES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
